aeb4964d0768a1904fece8f7ea12ce0c2203e857
Thishan Paramanayagam <jelamable@gmail.com>
2024-07-21 19:52:48 +0530

    Added Source Files

---
+++ src/menu-items/other.js
@@ -0,0 +1,33 @@
+// assets
+import { IconBrandChrome, IconHelp } from '@tabler/icons-react';
+
+// constant
+const icons = { IconBrandChrome, IconHelp };
+
+// ==============================|| SAMPLE PAGE & DOCUMENTATION MENU ITEMS ||============================== //
+
+const other = {
+  id: 'billCreation-docs-roadmap',
+  type: 'group',
+  children: [
+    {
+      id: 'billCreation-page',
+      title: 'Bill Creation Page',
+      type: 'item',
+      url: '/billCreation-page',
+      icon: icons.IconBrandChrome,
+      breadcrumbs: false
+    },
+    // {
+    //   id: 'documentation',
+    //   title: 'Documentation',
+    //   type: 'item',
+    //   url: 'https://codedthemes.gitbook.io/berry/',
+    //   icon: icons.IconHelp,
+    //   external: true,
+    //   target: true
+    // }
+  ]
+};
+
+export default other;
---
+++ src/views/billCreation-page/index.jsx
@@ -0,0 +1,15 @@
+// material-ui
+import Typography from '@mui/material/Typography';
+
+// project imports
+import MainCard from 'ui-component/cards/MainCard';
+
+// ==============================|| SAMPLE PAGE ||============================== //
+
+const BillCreation = () => (
+  <MainCard title="Enter Bill Details">
+
+  </MainCard>
+);
+
+export default BillCreation;
